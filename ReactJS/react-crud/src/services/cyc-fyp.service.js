2a9324bbad1bde19398a7440dff4c52ecd9a3276
20028846-Charmaine-Joson <20028846@myrp.edu.sg>
2022-06-08 11:50:40 +0800

    1st

--- ReactJS/react-crud/src/services/cyc-fyp.service.js
+++ ReactJS/react-crud/src/services/cyc-fyp.service.js
@@ -3,6 +3,33 @@ import http from "../http-common";
 class UserDataService {
   getAll() {
     return http.get("/users");
+  
   }
+
+}
+  class PlaygroundDataService {
+    getAll() {
+      return http.get("/playgrounds");
+    }
+  
+    get(id) {
+        return http.get(`/playgrounds/index/${id}`);
+    }
+  
+    create(data) {
+      return http.post("/playgrounds", data);
+    }
+  
+    update(id, data) {
+      return http.put(`/playgrounds/index/${id}`, data);
+    }
+  
+    delete(id) {
+      return http.delete(`/playgrounds/delete/${id}`);
+    }
+  
+    findByTitle(title) {
+      return http.get(`/playgrounds/search/${title}`);
+    }
 }
 export default new UserDataService();
